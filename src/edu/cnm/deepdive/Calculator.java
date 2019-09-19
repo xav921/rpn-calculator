724d6798c6eb4c1dea35c4c9a317ba7b604eefd4
xav921 <xav7470@gmail.com>
2019-09-19 09:21:16 -0600

    use try-with-resources

--- src/edu/cnm/deepdive/Calculator.java
+++ src/edu/cnm/deepdive/Calculator.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive;
 
+import java.io.InputStream;
+import java.io.Writer;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
@@ -7,31 +9,25 @@ import java.util.Scanner;
 
 public class Calculator {
 
-  private Scanner scanner;
-  private Deque<Double> operands;
-
   public static void main(String[] args) {
-    Calculator calc = new Calculator(new Scanner(System.in));
-    calc.process();
+    Calculator calc = new Calculator();
+    calc.process(System.in);
   }
 
-  public Calculator(Scanner scanner) {
-    this.scanner = scanner;
-    operands = new LinkedList<>();
-  }
-
-  public void process() {
-    try {
-      while (true){
-        if (scanner.hasNextDouble()) {
-          operands.push(scanner.nextDouble());
-        }
+  public void process(InputStream in) {
+    Deque<Double> operands = new LinkedList<>();
+    try (Scanner scanner = new Scanner(in)) {
+      while (scanner.hasNextDouble()) {
+        operands.push(scanner.nextDouble());
       }
     } catch (NoSuchElementException ignored) {
-
+      // End of input; complete processing
     } finally {
       System.out.println(operands);
+      if (scanner != null) {
+      scanner.close();
     }
+  }
   }
 
 }
